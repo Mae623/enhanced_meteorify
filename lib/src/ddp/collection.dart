4e09bd6538576219c144f864a583246214849426
梅宇 <1919612958@qq.com>
2022-10-08 17:53:35 +0800

    Update collection.dart

--- lib/src/ddp/collection.dart
+++ lib/src/ddp/collection.dart
@@ -21,7 +21,7 @@ Tuple2<String, Map<String, dynamic>> _parse(Map<String, dynamic> update) {
       if (update.containsKey('cleared')) {
         final List _updates = update['cleared'];
         final Map cleared = {};
-        _updates.map((e) => cleared[e.toString()] = null);
+        _updates.map((e) => cleared.addAll({e: null}));
         Logger().v(cleared);
         return Tuple2(_id, Map<String, dynamic>.from(cleared));
       }
