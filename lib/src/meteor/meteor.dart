d655f038c6152b095142cf01798de48d4a173b0b
梅宇 <1919612958@qq.com>
2022-10-24 09:33:08 +0800

    Update meteor.dart

--- lib/src/meteor/meteor.dart
+++ lib/src/meteor/meteor.dart
@@ -94,6 +94,7 @@ class Meteor {
     _enableLogs = enableLogs;
     var connectionStatus = await _connectToServer(url);
     _client!.removeStatusListener(_statusListener!);
+    return connectionStatus;
 
     var _token = await Utils.getString('token');
     _statusListener = (status) {
@@ -113,7 +114,6 @@ class Meteor {
       }
     };
     _client!.addStatusListener(_statusListener!);
-    return connectionStatus;
   }
 
   /// Connect to Meteor framework using the [url].
@@ -128,7 +128,7 @@ class Meteor {
     _connectionUrl = url;
     _client = DDP(_connectionUrl!, enableLogs: _enableLogs);
     _client!.connect();
-
+//fixme app启动后，连服务器，发生了什么
     _statusListener = (status) {
       if (status == ConnectStatus.connected) {
         isConnected = true;
